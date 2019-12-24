a334ebd02a5a5c61578825bdb3ad8aeea81eeb80
LawssssCat <1191693505@qq.com>
2019-12-24 10:03:45 +0800

    完成：整合ssm，可以开始前端开发

--- src/main/java/com/edut/controller/DoorController.java
+++ src/main/java/com/edut/controller/DoorController.java
@@ -1,7 +1,13 @@
 package com.edut.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.edut.dao.DoorMapper;
+import com.edut.pojo.Door;
 
 /**@Controller 注解的作用：
  * 1. 标识当前类属于 Controller 层
@@ -9,11 +15,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class DoorController {
+	/**
+	 * 自动装配：可以为当前变量赋值：到spring容器中根据类型匹配
+	 * DoorMapper 接口子类实例
+	 */
+	@Autowired
+	private DoorMapper doorMapperDao ; 
+	
 	
 	/**测试 SpringMVC 的开发环境
 	 * */
 	@RequestMapping("/test")
 	public String test() {
 		return "test";
+	}
+	
+	/**
+	 * 测试ssm的开发环境
+	 */
+	@RequestMapping("/testssm")
+	public String testssm() {
+		//查询所有门店信息
+		List<Door> list = doorMapperDao.findAll();
+		for (Door door : list) {
+			System.out.println(door);
+		}
+		
+		return "test" ; 
+	}
+	
+	
+	public String testAA() {
+		return "" ; 
 	}
 }
